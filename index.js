d9b2ecc361d5ae7af20762f78cb2930357d2be78
Russell Matney <russell.matney@gmail.com>
2015-04-13 02:37:36 -0400

    first implementation. no tests.

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 var Q = require('q');
+var fs = require('fs');
+var req = require('request');
 
 module.exports = function(event, options) {
+  var def = Q.defer();
+
+  var file = fs.createWriteStream(event.filepath);
+  file.on('finish', function() {
+    file.close(function(err){
+      if (err) {
+        def.reject(err);
+      } else {
+        def.resolve();
+      }
+    });
+  })
+  file.on('error', function(err) {
+    fs.unlink(event.filepath);
+    def.reject(err);
+  });
+
+  req(url).pipe(file);
+
+  return def.promise;
 }
+
